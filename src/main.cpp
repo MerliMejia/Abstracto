7feb2769ed4ef975ee3ff0916302c19ca32686a2
Merli Mejia <merlimejia25@gmail.com>
2026-03-08 18:13:38 -0400

    Refactoring 3 - ForwardRenderer

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,16 @@
 #include "backend/AppWindow.h"
 #include "backend/VulkanBackend.h"
 #include "renderable/DescriptorBindings.h"
-#include "renderable/Mesh.h"
 #include "renderable/FrameUniforms.h"
-#include "renderable/RenderUtils.h"
+#include "renderable/Mesh.h"
 #include "renderable/Sampler.h"
 #include "renderable/Texture.h"
-#include <array>
+#include "renderer/ForwardRenderer.h"
 #include <assert.h>
 #include <chrono>
 #include <cstdlib>
 #include <cstring>
-#include <fstream>
 #include <iostream>
-#include <stdexcept>
-#include <vector>
 
 #if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
 #include <vulkan/vulkan_raii.hpp>
@@ -48,18 +44,6 @@ private:
   BackendConfig backendConfig{.appName = "App Window",
                               .maxFramesInFlight = MAX_FRAMES_IN_FLIGHT};
 
-  vk::raii::DescriptorSetLayout descriptorSetLayout = nullptr;
-  vk::raii::PipelineLayout pipelineLayout = nullptr;
-  vk::raii::Pipeline graphicsPipeline = nullptr;
-
-  vk::raii::Image colorImage = nullptr;
-  vk::raii::DeviceMemory colorImageMemory = nullptr;
-  vk::raii::ImageView colorImageView = nullptr;
-
-  vk::raii::Image depthImage = nullptr;
-  vk::raii::DeviceMemory depthImageMemory = nullptr;
-  vk::raii::ImageView depthImageView = nullptr;
-
   DeviceContext &deviceContext() { return backend.device(); }
   SwapchainContext &swapchainContext() { return backend.swapchain(); }
   CommandContext &commandContext() { return backend.commands(); }
@@ -71,15 +55,14 @@ private:
   Sampler sampler;
   FrameUniforms frameUniforms;
   DescriptorBindings descriptorBindings;
+  ForwardRenderer forwardRenderer;
 
   void initWindow() { appWindow.create(WIDTH, HEIGHT, "App Window"); }
 
   void initVulkan() {
     backend.initialize(appWindow, backendConfig);
-    createDescriptorSetLayout();
-    createGraphicsPipeline();
-    createColorResources();
-    createDepthResources();
+    forwardRenderer.initialize(deviceContext(), swapchainContext(),
+                               SHADER_PATH);
     // Per texture init
     texture.create(TEXTURE_PATH, commandContext(), deviceContext());
     sampler.create(deviceContext());
@@ -89,8 +72,9 @@ private:
     mesh.createIndexBuffer(commandContext(), deviceContext());
     //................
     frameUniforms.create(deviceContext(), MAX_FRAMES_IN_FLIGHT);
-    descriptorBindings.create(deviceContext(), descriptorSetLayout, frameUniforms,
-                              texture, sampler, MAX_FRAMES_IN_FLIGHT);
+    descriptorBindings.create(
+        deviceContext(), forwardRenderer.descriptorSetLayout(), frameUniforms,
+        texture, sampler, MAX_FRAMES_IN_FLIGHT);
   }
 
   void mainLoop() {
@@ -106,287 +90,7 @@ private:
 
   void recreateSwapChain() {
     backend.recreateSwapchain(appWindow);
-    createColorResources();
-    createDepthResources();
-  }
-
-  void createDescriptorSetLayout() {
-    std::array bindings = {vk::DescriptorSetLayoutBinding(
-                               0, vk::DescriptorType::eUniformBuffer, 1,
-                               vk::ShaderStageFlagBits::eVertex, nullptr),
-                           vk::DescriptorSetLayoutBinding(
-                               1, vk::DescriptorType::eCombinedImageSampler, 1,
-                               vk::ShaderStageFlagBits::eFragment, nullptr)};
-
-    vk::DescriptorSetLayoutCreateInfo layoutInfo{
-        .bindingCount = static_cast<uint32_t>(bindings.size()),
-        .pBindings = bindings.data()};
-    descriptorSetLayout = vk::raii::DescriptorSetLayout(
-        deviceContext().deviceHandle(), layoutInfo);
-  }
-
-  void createGraphicsPipeline() {
-    vk::raii::ShaderModule shaderModule =
-        createShaderModule(readFile(SHADER_PATH));
-
-    vk::PipelineShaderStageCreateInfo vertShaderStageInfo{
-        .stage = vk::ShaderStageFlagBits::eVertex,
-        .module = shaderModule,
-        .pName = "vertMain"};
-    vk::PipelineShaderStageCreateInfo fragShaderStageInfo{
-        .stage = vk::ShaderStageFlagBits::eFragment,
-        .module = shaderModule,
-        .pName = "fragMain"};
-    vk::PipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo,
-                                                        fragShaderStageInfo};
-
-    auto bindingDescription = Vertex::getBindingDescription();
-    auto attributeDescriptions = Vertex::getAttributeDescriptions();
-    vk::PipelineVertexInputStateCreateInfo vertexInputInfo{
-        .vertexBindingDescriptionCount = 1,
-        .pVertexBindingDescriptions = &bindingDescription,
-        .vertexAttributeDescriptionCount =
-            static_cast<uint32_t>(attributeDescriptions.size()),
-        .pVertexAttributeDescriptions = attributeDescriptions.data()};
-    vk::PipelineInputAssemblyStateCreateInfo inputAssembly{
-        .topology = vk::PrimitiveTopology::eTriangleList,
-        .primitiveRestartEnable = vk::False};
-    vk::PipelineViewportStateCreateInfo viewportState{.viewportCount = 1,
-                                                      .scissorCount = 1};
-    vk::PipelineRasterizationStateCreateInfo rasterizer{
-        .depthClampEnable = vk::False,
-        .rasterizerDiscardEnable = vk::False,
-        .polygonMode = vk::PolygonMode::eFill,
-        .cullMode = vk::CullModeFlagBits::eBack,
-        .frontFace = vk::FrontFace::eCounterClockwise,
-        .depthBiasEnable = vk::False,
-        .lineWidth = 1.0f};
-    vk::PipelineMultisampleStateCreateInfo multisampling{
-        .rasterizationSamples = deviceContext().msaaSampleCount(),
-        .sampleShadingEnable = vk::False};
-    vk::PipelineDepthStencilStateCreateInfo depthStencil{
-        .depthTestEnable = vk::True,
-        .depthWriteEnable = vk::True,
-        .depthCompareOp = vk::CompareOp::eLess,
-        .depthBoundsTestEnable = vk::False,
-        .stencilTestEnable = vk::False};
-    vk::PipelineColorBlendAttachmentState colorBlendAttachment{
-        .blendEnable = vk::False,
-        .colorWriteMask =
-            vk::ColorComponentFlagBits::eR | vk::ColorComponentFlagBits::eG |
-            vk::ColorComponentFlagBits::eB | vk::ColorComponentFlagBits::eA};
-    vk::PipelineColorBlendStateCreateInfo colorBlending{
-        .logicOpEnable = vk::False,
-        .logicOp = vk::LogicOp::eCopy,
-        .attachmentCount = 1,
-        .pAttachments = &colorBlendAttachment};
-    std::vector dynamicStates = {vk::DynamicState::eViewport,
-                                 vk::DynamicState::eScissor};
-    vk::PipelineDynamicStateCreateInfo dynamicState{
-        .dynamicStateCount = static_cast<uint32_t>(dynamicStates.size()),
-        .pDynamicStates = dynamicStates.data()};
-
-    vk::PipelineLayoutCreateInfo pipelineLayoutInfo{
-        .setLayoutCount = 1,
-        .pSetLayouts = &*descriptorSetLayout,
-        .pushConstantRangeCount = 0};
-
-    pipelineLayout = vk::raii::PipelineLayout(deviceContext().deviceHandle(),
-                                              pipelineLayoutInfo);
-
-    vk::Format depthFormat = deviceContext().findDepthFormat();
-
-    vk::StructureChain<vk::GraphicsPipelineCreateInfo,
-                       vk::PipelineRenderingCreateInfo>
-        pipelineCreateInfoChain = {
-            {.stageCount = 2,
-             .pStages = shaderStages,
-             .pVertexInputState = &vertexInputInfo,
-             .pInputAssemblyState = &inputAssembly,
-             .pViewportState = &viewportState,
-             .pRasterizationState = &rasterizer,
-             .pMultisampleState = &multisampling,
-             .pDepthStencilState = &depthStencil,
-             .pColorBlendState = &colorBlending,
-             .pDynamicState = &dynamicState,
-             .layout = pipelineLayout,
-             .renderPass = nullptr},
-            {.colorAttachmentCount = 1,
-             .pColorAttachmentFormats =
-                 &swapchainContext().surfaceFormatInfo().format,
-             .depthAttachmentFormat = depthFormat}};
-
-    graphicsPipeline = vk::raii::Pipeline(
-        deviceContext().deviceHandle(), nullptr,
-        pipelineCreateInfoChain.get<vk::GraphicsPipelineCreateInfo>());
-  }
-
-  void createColorResources() {
-    vk::Format colorFormat = swapchainContext().surfaceFormatInfo().format;
-
-    RenderUtils::createImage(
-        deviceContext(), swapchainContext().extent2D().width,
-        swapchainContext().extent2D().height, 1,
-        deviceContext().msaaSampleCount(), colorFormat,
-        vk::ImageTiling::eOptimal,
-        vk::ImageUsageFlagBits::eTransientAttachment |
-            vk::ImageUsageFlagBits::eColorAttachment,
-        vk::MemoryPropertyFlagBits::eDeviceLocal, colorImage, colorImageMemory);
-    colorImageView = createImageView(colorImage, colorFormat,
-                                     vk::ImageAspectFlagBits::eColor, 1);
-  }
-
-  void createDepthResources() {
-    vk::Format depthFormat = deviceContext().findDepthFormat();
-
-    RenderUtils::createImage(
-        deviceContext(), swapchainContext().extent2D().width,
-        swapchainContext().extent2D().height, 1,
-        deviceContext().msaaSampleCount(), depthFormat,
-        vk::ImageTiling::eOptimal,
-        vk::ImageUsageFlagBits::eDepthStencilAttachment,
-        vk::MemoryPropertyFlagBits::eDeviceLocal, depthImage, depthImageMemory);
-    depthImageView = createImageView(depthImage, depthFormat,
-                                     vk::ImageAspectFlagBits::eDepth, 1);
-  }
-
-  [[nodiscard]] vk::raii::ImageView
-  createImageView(const vk::raii::Image &image, vk::Format format,
-                  vk::ImageAspectFlags aspectFlags, uint32_t mipLevels) {
-    vk::ImageViewCreateInfo viewInfo{
-        .image = image,
-        .viewType = vk::ImageViewType::e2D,
-        .format = format,
-        .subresourceRange = {aspectFlags, 0, mipLevels, 0, 1}};
-    return vk::raii::ImageView(deviceContext().deviceHandle(), viewInfo);
-  }
-
-  void recordCommandBuffer(uint32_t frameIndex, uint32_t imageIndex) {
-    auto &commandBuffer = commandContext().commandBuffer(frameIndex);
-    commandBuffer.begin({});
-    // Before starting rendering, transition the swapchain image to
-    // COLOR_ATTACHMENT_OPTIMAL
-    transition_image_layout(
-        commandBuffer, swapchainContext().swapchainImages()[imageIndex],
-        vk::ImageLayout::eUndefined, vk::ImageLayout::eColorAttachmentOptimal,
-        {}, // srcAccessMask (no need to wait for previous operations)
-        vk::AccessFlagBits2::eColorAttachmentWrite,         // dstAccessMask
-        vk::PipelineStageFlagBits2::eColorAttachmentOutput, // srcStage
-        vk::PipelineStageFlagBits2::eColorAttachmentOutput, // dstStage
-        vk::ImageAspectFlagBits::eColor);
-    // Transition the multisampled color image to COLOR_ATTACHMENT_OPTIMAL
-    transition_image_layout(commandBuffer, *colorImage,
-                            vk::ImageLayout::eUndefined,
-                            vk::ImageLayout::eColorAttachmentOptimal,
-                            vk::AccessFlagBits2::eColorAttachmentWrite,
-                            vk::AccessFlagBits2::eColorAttachmentWrite,
-                            vk::PipelineStageFlagBits2::eColorAttachmentOutput,
-                            vk::PipelineStageFlagBits2::eColorAttachmentOutput,
-                            vk::ImageAspectFlagBits::eColor);
-    // Transition the depth image to DEPTH_ATTACHMENT_OPTIMAL
-    transition_image_layout(commandBuffer, *depthImage,
-                            vk::ImageLayout::eUndefined,
-                            vk::ImageLayout::eDepthAttachmentOptimal,
-                            vk::AccessFlagBits2::eDepthStencilAttachmentWrite,
-                            vk::AccessFlagBits2::eDepthStencilAttachmentWrite,
-                            vk::PipelineStageFlagBits2::eEarlyFragmentTests |
-                                vk::PipelineStageFlagBits2::eLateFragmentTests,
-                            vk::PipelineStageFlagBits2::eEarlyFragmentTests |
-                                vk::PipelineStageFlagBits2::eLateFragmentTests,
-                            vk::ImageAspectFlagBits::eDepth);
-
-    vk::ClearValue clearColor = {
-        .color = {.float32 = std::array<float, 4>{0.0f, 0.0f, 0.0f, 1.0f}}};
-    vk::ClearValue clearDepth = {.depthStencil =
-                                     vk::ClearDepthStencilValue{1.0f, 0}};
-
-    // Color attachment (multisampled) with resolve attachment
-    vk::RenderingAttachmentInfo colorAttachment = {
-        .imageView = colorImageView,
-        .imageLayout = vk::ImageLayout::eColorAttachmentOptimal,
-        .resolveMode = vk::ResolveModeFlagBits::eAverage,
-        .resolveImageView =
-            swapchainContext().swapchainImageViews()[imageIndex],
-        .resolveImageLayout = vk::ImageLayout::eColorAttachmentOptimal,
-        .loadOp = vk::AttachmentLoadOp::eClear,
-        .storeOp = vk::AttachmentStoreOp::eStore,
-        .clearValue = clearColor};
-
-    // Depth attachment
-    vk::RenderingAttachmentInfo depthAttachment = {
-        .imageView = depthImageView,
-        .imageLayout = vk::ImageLayout::eDepthAttachmentOptimal,
-        .loadOp = vk::AttachmentLoadOp::eClear,
-        .storeOp = vk::AttachmentStoreOp::eDontCare,
-        .clearValue = clearDepth};
-
-    vk::RenderingInfo renderingInfo = {
-        .renderArea = {.offset = {0, 0},
-                       .extent = swapchainContext().extent2D()},
-        .layerCount = 1,
-        .colorAttachmentCount = 1,
-        .pColorAttachments = &colorAttachment,
-        .pDepthAttachment = &depthAttachment};
-    commandBuffer.beginRendering(renderingInfo);
-    commandBuffer.bindPipeline(vk::PipelineBindPoint::eGraphics,
-                               *graphicsPipeline);
-    commandBuffer.setViewport(
-        0,
-        vk::Viewport(0.0f, 0.0f,
-                     static_cast<float>(swapchainContext().extent2D().width),
-                     static_cast<float>(swapchainContext().extent2D().height),
-                     0.0f, 1.0f));
-    commandBuffer.setScissor(
-        0, vk::Rect2D(vk::Offset2D(0, 0), swapchainContext().extent2D()));
-    commandBuffer.bindVertexBuffers(0, *mesh.getVertexBuffer(), {0});
-    commandBuffer.bindIndexBuffer(*mesh.getIndexBuffer(), 0,
-                                  vk::IndexType::eUint32);
-    commandBuffer.bindDescriptorSets(vk::PipelineBindPoint::eGraphics,
-                                     pipelineLayout, 0,
-                                     *descriptorBindings.descriptorSet(frameIndex),
-                                     nullptr);
-    commandBuffer.drawIndexed(mesh.getIndices().size(), 1, 0, 0, 0);
-    commandBuffer.endRendering();
-    // After rendering, transition the swapchain image to PRESENT_SRC
-    transition_image_layout(
-        commandBuffer, swapchainContext().swapchainImages()[imageIndex],
-        vk::ImageLayout::eColorAttachmentOptimal,
-        vk::ImageLayout::ePresentSrcKHR,
-        vk::AccessFlagBits2::eColorAttachmentWrite,         // srcAccessMask
-        {},                                                 // dstAccessMask
-        vk::PipelineStageFlagBits2::eColorAttachmentOutput, // srcStage
-        vk::PipelineStageFlagBits2::eBottomOfPipe,          // dstStage
-        vk::ImageAspectFlagBits::eColor);
-    commandBuffer.end();
-  }
-
-  void transition_image_layout(vk::raii::CommandBuffer &commandBuffer,
-                               vk::Image image, vk::ImageLayout old_layout,
-                               vk::ImageLayout new_layout,
-                               vk::AccessFlags2 src_access_mask,
-                               vk::AccessFlags2 dst_access_mask,
-                               vk::PipelineStageFlags2 src_stage_mask,
-                               vk::PipelineStageFlags2 dst_stage_mask,
-                               vk::ImageAspectFlags image_aspect_flags) {
-    vk::ImageMemoryBarrier2 barrier = {
-        .srcStageMask = src_stage_mask,
-        .srcAccessMask = src_access_mask,
-        .dstStageMask = dst_stage_mask,
-        .dstAccessMask = dst_access_mask,
-        .oldLayout = old_layout,
-        .newLayout = new_layout,
-        .srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
-        .dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
-        .image = image,
-        .subresourceRange = {.aspectMask = image_aspect_flags,
-                             .baseMipLevel = 0,
-                             .levelCount = 1,
-                             .baseArrayLayer = 0,
-                             .layerCount = 1}};
-    vk::DependencyInfo dependency_info = {.dependencyFlags = {},
-                                          .imageMemoryBarrierCount = 1,
-                                          .pImageMemoryBarriers = &barrier};
-    commandBuffer.pipelineBarrier2(dependency_info);
+    forwardRenderer.recreate(deviceContext(), swapchainContext(), SHADER_PATH);
   }
 
   void updateUniformBuffer(uint32_t currentImage) {
@@ -417,35 +121,13 @@ private:
       return;
     }
     updateUniformBuffer(frameState->frameIndex);
-    recordCommandBuffer(frameState->frameIndex, frameState->imageIndex);
+    forwardRenderer.record(
+        commandContext().commandBuffer(frameState->frameIndex),
+        swapchainContext(), mesh, descriptorBindings, frameState->frameIndex,
+        frameState->imageIndex);
     if (backend.endFrame(*frameState, appWindow)) {
       recreateSwapChain();
     }
-  }
-
-  [[nodiscard]] vk::raii::ShaderModule
-  createShaderModule(const std::vector<char> &code) {
-    vk::ShaderModuleCreateInfo createInfo{
-        .codeSize = code.size(),
-        .pCode = reinterpret_cast<const uint32_t *>(code.data())};
-    vk::raii::ShaderModule shaderModule{deviceContext().deviceHandle(),
-                                        createInfo};
-
-    return shaderModule;
-  }
-
-  static std::vector<char> readFile(const std::string &filename) {
-    std::ifstream file(filename, std::ios::ate | std::ios::binary);
-
-    if (!file.is_open()) {
-      throw std::runtime_error("failed to open file!");
-    }
-    std::vector<char> buffer(file.tellg());
-    file.seekg(0, std::ios::beg);
-    file.read(buffer.data(), static_cast<std::streamsize>(buffer.size()));
-    file.close();
-
-    return buffer;
   }
 };
 
